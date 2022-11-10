723a1a8e74dfa75e495f14f61c8e5e0dd0609e95
Sunday <sunepa091522@gmail.com>
2022-11-10 17:17:47 +0100

    initial commit

---
+++ chapterThree/Account.java
@@ -0,0 +1,29 @@
+public class Account{
+
+	private String name;
+	private double balance
+
+
+	public Account(){
+	}
+
+	public Account(String name, double balance){
+		this.name = name;
+		this.balance = balance;
+
+	}
+
+	public void setName(String name){
+		this. name = name;
+
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public double getBalance(){
+		return balance;
+
+
+}
